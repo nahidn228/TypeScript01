e6ee384fa4069a39d5353f6b42090548a1b056f3
nahidn228 <nahidn228@gmail.com>
2025-05-01 09:03:42 +0600

    call method in typeScript

--- module1/src/1.6.ts
+++ module1/src/1.6.ts
@@ -9,41 +9,46 @@ add(2 + 2);
 const add = (num1: number, num2: number): number => num1 + num2;
 
 
+// object ---> function ---> Method
+
+
+const poorUser = {
+  name: "nahid",
+  balance: 100,
+  addFunction(balance: number): number {
+    return `My balance is ${this.balance + balance}`;
+  },
+};
 
 
 
 
 
 
+// {
+//   //Learning function
+//   // Normal Function
+//   // Arrow Function
 
+//   function add(num1: number, num2: number = 10): number {
+//     return num1 + num2;
+//   }
 
+//   add(2, "2");
 
+//   const addArrow = (num1: number, num2: number): number => num1 + num2;
 
+//   // object --> function --> method
 
-/ {
-    //   //Learning function
-    //   // Normal Function
-    //   // Arrow Function
-    
-    //   function add(num1: number, num2: number = 10): number {
-    //     return num1 + num2;
-    //   }
-    
-    //   add(2, "2");
-    
-    //   const addArrow = (num1: number, num2: number): number => num1 + num2;
-    
-    //   // object --> function --> method
-    
-    //   const poorUser = {
-    //     name: "Mezba",
-    //     balance: 0,
-    //     addBalance(balance: number): string {
-    //       return `My new balance is : ${this.balance + balance}`;
-    //     },
-    //   };
-    
-    //   const arr: number[] = [1, 4, 10];
-    
-    //   const newArray: number[] = arr.map((elem: number): number => elem * elem);
-    // }
+//   const poorUser = {
+//     name: "Mezba",
+//     balance: 0,
+//     addBalance(balance: number): string {
+//       return `My new balance is : ${this.balance + balance}`;
+//     },
+//   };
+
+//   const arr: number[] = [1, 4, 10];
+
+//   const newArray: number[] = arr.map((elem: number): number => elem * elem);
+// }

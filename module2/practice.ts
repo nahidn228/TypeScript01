21905fda97e1c5b9b5a8073ac9430f9525ab774c
nahidn228 <nahidn228@gmail.com>
2025-05-05 09:15:10 +0600

    explore type guards

---
+++ module2/practice.ts
@@ -0,0 +1,44 @@
+{
+  //
+
+/**
+Task 1: Basic Data Types and First Program
+Objective: Write a TypeScript program that outputs a welcome message.
+
+Instructions:
+
+Create a TypeScript program.
+Print the following message to the console:
+Hello World, I will complete this course successfully and become a Next level Web Developer!
+  
+ */
+
+  const welcome = () => {
+    console.log(
+      "Hello World, I will complete this course successfully and become a Next level Web Developer!"
+    );
+  };
+  welcome();
+
+
+  /**
+Task 2: Functions, Optional, and Literal Types
+Objective: Create a function with parameters and an optional literal type.
+
+Instructions:
+
+Define a function that takes:
+name (string)
+age (number)
+role (optional, with type 'admin' | 'user' | 'guest')
+The function should log these values or perform a basic action.
+
+  
+ */
+
+
+
+
+
+  //
+}
